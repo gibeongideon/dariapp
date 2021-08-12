1082b0bfc6c63f76dc52fd030918cbbcf445690a
gibeongideon <kipngeno.gibeon@gmail.com>
2021-08-12 18:22:06 +0300

    add_epesa_FE

--- account/template_views.py
+++ account/template_views.py
@@ -64,11 +64,11 @@ def mpesa_deposit(request):
     )
 
 
-# # Use redis cashing here for speed
-# @login_required(login_url="/users/login")
-# def trans_log(request):
-#     trans_logz = TransactionLog.objects.filter(user=request.user)
-#     return render(request, "account/trans_log.html", {"trans_logz": trans_logz})
+# Use redis cashing here for speed
+@login_required(login_url="/users/login")
+def trans_log(request):
+    trans_logz = TransactionLog.objects.filter(user=request.user)
+    return render(request, "account/trans_log.html", {"trans_logz": trans_logz})
 
 
 @login_required(login_url="/user/login")
