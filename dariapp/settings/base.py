eee1a68cbb70309e08b1f6a2cbb094b4846a15d9
gibeongideon <kipngeno.gibeon@gmail.com>
2021-08-20 18:09:19 +0300

    account_modal_fix_links

--- dariapp/settings/base.py
+++ dariapp/settings/base.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = [BASE_DIR / 'staticfiles',]
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/3.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
