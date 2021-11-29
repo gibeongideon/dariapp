4975c2b3a855b4e7b56203a802b0ced290411568
gibeongideon <kipngeno.gibeon@gmail.com>
2021-11-29 15:42:19 +0300

    pro_settings_update

--- dariapp/settings/dev.py
+++ dariapp/settings/dev.py
@@ -1,13 +1,13 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True#config("DEBUG", default=True, cast=bool)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-cbk=3tx++3-x+1$ohy2g960+o$+f1y5*cv4o*mrp-hphmxgc8p'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['0.0.0.0', '.herokuapp.com', '127.0.0.1'] 
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['95.217.221.199', 'www.darispin.ga', '127.0.0.1'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
@@ -17,13 +17,13 @@ try:
 except ImportError:
     pass
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": config("DB_NAME", default="dariusdb"),
-#         "USER": config("DB_USER", default="darius"),
-#         "PASSWORD": config("DB_PASSWORD", default="darius54321"),
-#         "HOST": "localhost",
-#         "PORT": "",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_NAME", default="dariusdb"),
+        "USER": config("DB_USER", default="darius"),
+        "PASSWORD": config("DB_PASSWORD", default="darius54321"),
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
--- dariapp/settings/production.py
+++ dariapp/settings/production.py
@@ -6,9 +6,9 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 SECRET_KEY = 'django-insecure-cbk=3tx++3-x+1$ohy2g960+o$+f1y5*cv4o*mrp-hphmxgc8p'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['0.0.0.0', '.herokuapp.com', '127.0.0.1'] 
-
+ALLOWED_HOSTS = ['95.217.221.199', 'www.darispin.ga', '127.0.0.1'] 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
