6534ca7cccf83326c7528afd0e0e5af30f9b1910
gibeongideon <kipngeno.gibeon@gmail.com>
2022-02-20 12:20:32 +0300

    production_settings

--- dariapp/settings/dev.py
+++ dariapp/settings/dev.py
@@ -7,9 +7,9 @@ SECRET_KEY = 'dariappdjango-insecure-cbk=3tx++3-x+1$ohy2g960+o$+f1y5*cv4o*mrp-hp
 
 # SECURITY WARNING: define the correct hosts in production!
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['159.223.9.47', 'www.dariplay.ga', '127.0.0.1'] 
+ALLOWED_HOSTS = ['159.223.9.47', 'www.dariplay.ga', 'dariplay.ga','127.0.0.1'] 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 try:
