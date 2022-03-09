1a59a8993fc7736cb6aa5d71458b0b10e9499fd6
gibeongideon <kipngeno.gibeon@gmail.com>
2022-03-10 00:09:12 +0300

    ilter_html_elements_message)#avoid XSS attack

--- home/models.py
+++ home/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 
+def filter_html_elements(content):
+    #TODO
+    return content
+
+
 class Contact(models.Model):
     cmail =  models.CharField(max_length=100, blank=True, null=True)
     message = models.TextField(max_length=100, blank=True, null=True)
 
     class Meta:
         db_table = "d_contact_us"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.message=filter_html_elements(self.message)#avoid XSS attack 
+            super().save(*args, **kwargs)
+        else:
+            return    
